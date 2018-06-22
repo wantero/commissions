a2bc5f42fae2bfabacf65f5c2316330c27ea30ba
wantero <wantero@dlctec.com.br>
2018-06-22 16:15:27 -0300

    Auth

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,16 +1,28 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function(server) {
 
-    //API Routes
-    const router = express.Router()
-    server.use('/api', router)
+    //API Routes protected by Token JWT
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+
+    protectedApi.use(auth)
 
     //USER Routes
     const userService = require('../api/user/userService')
-    userService.register(router, '/user')
+    userService.register(protectedApi, '/user')
 
     //VENDOR Routes
     const vendorService = require('../api/vendor/vendorService')
-    vendorService.register(router, '/vendor')
+    vendorService.register(protectedApi, '/vendor')
+
+    //Open API Routes
+    const openApi = express.Router()     
+    server.use('/oapi', openApi)
+       
+    const AuthService = require('../api/user/AuthService')     
+    openApi.post('/login', AuthService.login)     
+    openApi.post('/signup', AuthService.signup)     
+    openApi.post('/validateToken', AuthService.validateToken)     
 }
