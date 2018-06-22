9870e6b3c5a71c55755bcb2bf6b52dedcf40be95
wantero <wantero@dlctec.com.br>
2018-06-22 00:13:27 -0300

    errorHandler

--- src/api/vendor/vendorService.js
+++ src/api/vendor/vendorService.js
@@ -1,6 +1,8 @@
 const Vendor = require('./vendor')
+const errorHandler = require('../common/errorHandler')
 
 Vendor.methods(['get','post','put','delete'])
 Vendor.updateOptions({new: true, runValidators: true})
+Vendor.after('post', errorHandler).after('put', errorHandler)
 
 module.exports = Vendor
